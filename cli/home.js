d22d6785aaa7ff264a0ce49ef04757e445810105
ilorez <zobirnajdaoui@gmail.com>
2024-04-01 20:10:15 +0000

    bring some image functions to live

--- cli/home.js
+++ cli/home.js
@@ -3,6 +3,7 @@ import { createContainer, deleteContainer, getContainers, runContainer, stopCont
 import colors from 'colors';
 import readline from 'readline'
 import { setTimeout } from 'timers/promises';
+import { deleteImage, getImages } from '../functions/images.js';
 
 // Create readline interface
 const rl = readline.createInterface({
@@ -49,13 +50,21 @@ export async function home(){
 
   if(section.action === sectionsObj.containers){
     containersSection()
-  }  
+  }else if(section.action === sectionsObj.images){
+    imagesSection()
+  }
+}
+
+const createTable = (headerArray) =>{
+  console.clear()
+  let header = headerArray.map(h=>field(h,15)).join(" | ")
+  console.log(header)
+  console.log("_".repeat(header.length))
 }
 
 const containersSection=()=>{
-  console.clear()
-  console.log("  "+field("Name",15)+" | "+field("Image",15)+" | "+field("Status",9)+" | "+field("Port",15))
-  console.log("______________________________________________________")
+  let containerHeader = ["Name","Image","Status","Port"]
+  createTable(containerHeader) 
   getContainers({all:true}).then(async(containers) => {
     const table = containers.map(c=>[
       field(c.Names[0].slice(1),15),
@@ -92,7 +101,8 @@ const containersSection=()=>{
         containersSection()
         break;
       case "stop":
-        await stopContainer(Selectcontainers.action)
+        await stopContainer(Selectcontainers.action).then(()=>{
+          console.log("Container stopped")})
         await setTimeout(()=>{},3000)
         containersSection()
         break;
@@ -114,21 +124,64 @@ const containersSection=()=>{
         containersSection()
         break;
     }
-
-
-
-
-
-
-
   })
-
-  
-
-
-
-
 }
+
+const imagesSection=()=>{
+  let imagesHeader = ["Name","Size"]
+  createTable(imagesHeader) 
+  getImages().then(async(images) => {
+    const table = images.map(c=>[
+      field(c.RepoTags[0],15),
+      field(c.Size,15),
+    ].join(" | "))
+
+    const Selectcontainers = await inquirer.prompt({
+        type:"list",
+        name: "action",
+        message : " ",
+        choices:images.map((_,i)=>({name:table[i],value:_.Id})),
+        pageSize:15,
+        loop:false,
+      })
+    
+    const action = await inquirer.prompt({
+      type:"list",
+      name: "action",
+      message : " ",
+      choices:[
+        {name: "Create container", value: "create"},
+        {name:"\uf1f8 Delete".red,value:"delete"},
+        new inquirer.Separator(),
+        {name:"Quit",value:"quit"},
+      ]
+    })
+
+    switch(action.action){
+      case "create":
+        await createContainer(Selectcontainers.action)
+        imagesSection()
+        break;
+      case "delete":
+        const confirm = await inquirer.prompt({
+          type:"confirm",
+          name: "action",
+          message : "Are you sure you want to delete this image?",
+        })
+        if(!confirm.action){
+          imagesSection()
+          break;
+        }
+        await deleteImage(Selectcontainers.action)
+        await setTimeout(()=>{},3000)
+        imagesSection()
+        break;
+      case "quit":
+        imagesSection()
+        break;
+    }
+  
+  })}
 
 const field = (value,length) =>{
   if(value.length > length){
