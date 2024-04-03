1e87bd1e806e9cfdda8105cd283169b0b2d5af54
ilorez <zobirnajdaoui@gmail.com>
2024-04-03 20:26:44 +0000

    create function createContainer,getImageName generateContainerNameFromImageName

--- functions/images.js
+++ functions/images.js
@@ -1,5 +1,6 @@
 const docker = new Docker();
 import  Docker from 'dockerode'
+import { getContainers } from './containers.js';
 
 const getImages = async () => {
     try {
@@ -31,10 +32,45 @@ const deleteImage = async (id) => {
     }
 }
 
+const createContainer = async (image,containerName='containerName') => {
+    try {
+      const container = await docker.createContainer({
+        Image: image,
+        AttachStdout: true,
+        AttachStderr: true,
+        Tty: true,
+      });
+      await container.rename({name:containerName});
+    //   set container  Names[0] =containerName 
+      await 
+      await container.start();
+    } catch (err) {
+      return err;
+    }
+  }
 
+//   get image name 
+const getImageName = async (id) => {
+    try {
+        const image = docker.getImage(id);
+        const imageInfo = await image.inspect();
+        return imageInfo.RepoTags[0];
+    } catch (err) {
+        return err
+    }
+}
+// generateContainerNameFromImageName that takes an image name as an argument and returns a container name based on the image name. The container name should be the same as the image name, but with the prefix "container-" added to it. For example, if the image name is "nginx", the container name should be "container-nginx" and random number between 1-9999
+const generateContainerNameFromImageName =  (imageName) => {
+    const random = Math.floor(Math.random() * 9999) + 1;
+    const newImageName = imageName.replace(/[^a-zA-Z0-9]/g, '');
+    return `c-${newImageName}-${random}`;
+    }
 
 export {
     getImages,
     createImage,
-    deleteImage
+    createContainer,
+    generateContainerNameFromImageName,
+    deleteImage,
+    getImageName
 }
